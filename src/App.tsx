05bd556871c4fb4b07e34626eff0af43ba27799a
Vadim Suharnikov <vsuahrnikov@gmail.com>
2024-09-17 07:16:19 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "@radix-ui/themes/styles.css" with { type: "css" };
+import { Button, Theme } from "@radix-ui/themes";
+
+function App() {
+  return (
+    <Theme>
+      <Button>Hey 👋</Button>
+    </Theme>
+  );
+}
+
+export default App;
